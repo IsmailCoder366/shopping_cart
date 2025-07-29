6fd67358b1364b49c52763f228ad3cc46b91df5b
Muhammad Ismail <ismailcoder366@gmail.com>
2025-07-29 20:19:13 +0500

    ndk version added

--- lib/product_list.dart
+++ lib/product_list.dart
@@ -9,6 +9,22 @@ class ProductList extends StatefulWidget {
 }
 
 class _ProductListState extends State<ProductList> {
+
+  List<String> productName = ['Mango', 'Orange', 'Apple', 'Banana', 'Grapes'];
+
+  List<String> productUnit = ['kg', 'Dozen', 'kg', 'Dozen', 'kg'];
+
+  List<int> productPrice = [100, 200, 300, 400, 500];
+
+  List<Image> productImage = [
+    Image.network('https://media.istockphoto.com/id/1019835828/photo/mango-and-leaf-isolated-white-background.jpg?s=612x612&w=0&k=20&c=_nmOBzO9mGEitT2rUvO1xAX9jwL5mHYI8AFRbYeyy-A='),
+    Image.network('https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcSm-IP0vGHOYDQUqEokK1k-j5GGBh9Af-XWDw&s'),
+    Image.network('https://media.istockphoto.com/id/184276818/photo/red-apple.jpg?s=612x612&w=0&k=20&c=NvO-bLsG0DJ_7Ii8SSVoKLurzjmV0Qi4eGfn6nW3l5w='),
+    Image.network('https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcRZdYAUXvlwNIrC2A_4oIhFUyBtKtLNHm0buQ&s'),
+    Image.network('https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcS8UtrpPyYy_9wEWxUEybi3stLiEg3sSEfgiQ&s'),
+
+  ];
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
@@ -37,6 +53,34 @@ class _ProductListState extends State<ProductList> {
           SizedBox(width: 20,)
         ],
       ),
-    );
+
+      body: Column(
+        children: [
+          Expanded(
+
+                child: ListView.builder(
+
+              itemCount: productName.length,
+              itemBuilder: (context, index) {
+                return Card(
+                  child: Column(
+                    children: [
+                      Row(
+                        children: [
+                            Image(
+                                height : 100,
+                                width: 100,
+                                image: NetworkImage(productImage[index].toString())),
+
+                        ],
+                      )
+                    ],
+                  ),
+                );
+              }))
+
+        ],
+      )
+      );
   }
 }
